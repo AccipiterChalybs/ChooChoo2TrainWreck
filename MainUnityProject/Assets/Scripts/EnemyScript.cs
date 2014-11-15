413fd90b5655cb93ca15b8e5711dc6ce1de4d584
David Ruble <david.ruble1@yahoo.com>
2014-11-14 21:21:34 -0800

    fixed enemy movement respawn timer

--- MainUnityProject/Assets/Scripts/EnemyScript.cs
+++ MainUnityProject/Assets/Scripts/EnemyScript.cs
@@ -10,21 +10,23 @@ public class EnemyScript : MonoBehaviour {
 	public float speed;
 
 	private bool isMoving;
+	private bool respawning;
+	private float respawnTimer;
 	private Vector3 respawnLocation;
 
 	// Use this for initialization
 	void Start () 
 	{
 		respawnLocation = transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		moveEnemy ();
+		respawnTimer = delay; //allow moving when game first starts
 	}
 
-	void moveEnemy()
+	void FixedUpdate () 
+	{
+		MoveEnemy ();
+	}
+
+	void MoveEnemy()
 	{
 		switch(moveDirection)
 		{
@@ -32,57 +34,69 @@ public class EnemyScript : MonoBehaviour {
 			if (transform.position.x > endValue)
 				isMoving = true;
 			else
+			{
 				isMoving = false;
+				respawnTimer = 0.0f;
+			}
 			break;
 		case Direction.Right:
 			if (transform.position.x < endValue)
 				isMoving = true;
 			else
+			{
 				isMoving = false;
+				respawnTimer = 0.0f;
+			}
 			break;
 		case Direction.Forward:
 			if (transform.position.z < endValue)
 				isMoving = true;
 			else
+			{
 				isMoving = false;
+				respawnTimer = 0.0f;
+			}
 			break;
 		case Direction.Backward:
 			if (transform.position.z > endValue)
 				isMoving = true;
 			else
+			{
 				isMoving = false;
+				respawnTimer = 0.0f;
+			}
 			break;
 		case Direction.Up:
 			if (transform.position.y < endValue)
 				isMoving = true;
 			else
+			{
 				isMoving = false;
+				respawnTimer = 0.0f;
+			}
 			break;
 		case Direction.Down:
 			if (transform.position.y > endValue)
 				isMoving = true;
 			else
+			{
 				isMoving = false;
+				respawnTimer = 0.0f;
+			}
 			break;
 		default:
 			print ("unimplemented move");
 			break;
 		}
 		
-		if (isMoving)
+		if (isMoving && respawnTimer >= delay)
 		{
 			transform.Translate (Vector3.forward * Time.deltaTime * speed, Space.Self);
 		}
 		else
 		{
 			transform.position = respawnLocation;
-			StartCoroutine(WaitForRespawn());
+			respawnTimer += Time.deltaTime;
 		}
 	}
-
-	IEnumerator WaitForRespawn() 
-	{
-		yield return new WaitForSeconds(delay);
-	}
-
 }
