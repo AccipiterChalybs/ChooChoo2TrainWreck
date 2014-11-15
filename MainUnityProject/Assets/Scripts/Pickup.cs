b7b14429489b8d2e3b6daa23ea291cf88f77e03a
Kamashamasay <tmctigue@mindspring.com>
2014-11-14 22:05:01 -0800

    Pickup changes

--- MainUnityProject/Assets/Scripts/Pickup.cs
+++ MainUnityProject/Assets/Scripts/Pickup.cs
@@ -3,7 +3,45 @@ using System.Collections;
 
 public class Pickup : MonoBehaviour {
 	/* should just call onPickup, which will be overriden for each powerup */
-	void OnCollisionEnter( Collision obj);
+	//Make sure to distinguish which player picks it up
+	void OnCollisionEnter( Collision obj)
+	{
+		if(obj.gameObject.GetComponent<ControllablePlayer> != null){
+				this.onPickup (obj.gameObject.GetComponent<ControllablePlayer> ());
+			//calling on pickup on this passing a referance to controllable player
+			}
+	}
+	void onPickup()
+	{
+		//needs to find the identity of the pickupable object/power up
+		//obj.gameObject.GetName<Pickup object>
 
-	void onPickup();
+		//star
+			//immunity for 10 seconds
+				//if there is a talent system this can either 
+					//increase to 20 seconds or 
+					//have 90 seconds of projectile protection or
+					
+					// more common drop timer (something to affect type of drops given
+		//shield
+			//absorbs a certain amount of damage
+				//similair to star can be better or worse
+			
+		//wind, blows away enemies
+
+		//magnet, pulls power ups 3 blocks away towards frogger
+
+		//Hearts finish the level, collecting different types 
+			//froggers share heart pieces
+			
+			//are heart pieces randomly generated or not?
+				//if heart pieces have fixed locations do the types of heart pieces at different locations change?
+
+			
+
+		//Movements speed increase?, speed up the jump speed.
+			//this would increase the speed after ray is called
+
+		//
+	}
 }
