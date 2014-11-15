5a45f0e573a0655a61d7ca32b07d3fbacd292373
AccipiterChalybs <pumpkinpi64@gmail.com>
2014-11-14 19:52:08 -0800

    Added Missing File

--- MainUnityProject/Assets/Scripts/InputManager.cs
+++ MainUnityProject/Assets/Scripts/InputManager.cs
@@ -2,14 +2,31 @@
 using System.Collections;
 
 public class InputManager : MonoBehaviour {
+	public string[] buttonList;
+	public ControllablePlayer currentPlayer; //player only
+	public NetworkManager networkManager;
+
 	/* Search for input on update by calling helper function*/
-	void Update();    
+	void Update() {
+		CheckInput ();
+	}
 	
 	/* (P only) Checks for input */
-	void CheckInput();
+	void CheckInput() {
+		foreach (string button in buttonList) {
+			InputAction action = new InputAction();
+			action.buttonName = button;
+			action.value = Input.GetAxis(button);
+			ApplyInput (currentPlayer, action);
+		}
+		//networkManager.send
+	}
+
 	
 	/* (P only) Applies Input to connected player */
-	void ApplyInput();
+	void ApplyInput(ControllablePlayer player, InputAction action) {
+		player.AddMove(action);
+	}
 	
 	/* packages needed input to client_SendInput() */
 	void SendInput();
