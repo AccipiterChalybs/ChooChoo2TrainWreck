edae55ea6ad2532f43d73ed4d46a2164ef860228
AccipiterChalybs <pumpkinpi64@gmail.com>
2014-11-14 18:43:58 -0800

    Initial functions for some files.

--- MainUnityProject/Assets/Scripts/ControllablePlayer.cs
+++ MainUnityProject/Assets/Scripts/ControllablePlayer.cs
@@ -1,15 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ControllablePlayer : MonoBehaviour {
+	public enum Effect {Slow, NoMove, Stun};
+	public float baseSpeed; //hops / second
+	private InputAction mCurrentAction;
+	private InputAction mQueuedAction;
+	//private auto lastUpdateTime;
+	private Vector3 actualPosition; //non-visual position
+	private Vector3 destination;
+	private Vector3 midpoint;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+	/* Called when object is created, like ctor 
+	     * Leave this alone for now. */
+	void Start();    
+
+	/* Called every frame.
+	     *  Handle updating player's position & other visual changes, as these
+	     *  need to be done each frame to avoid jerky-ness. However, actions
+	     *  should *not* be done here, as otherwise low FPS might lead to more
+	     *  de-synchonization other the internet. */
+	void Update();
+
+	/* Called on current Player - moves player from actual position by
+	     * ([CurrentTime] - lastUpdateTime) */
+	void extrapolatePlayer(); 
+
+	/* Called on other players, interpolates between snapshots */
+	void interpolatePlayer();
+
+	/* Called at a certain rate every second.
+	     *  Handle actions and other gameplay stuff here 
+	     *  (like decrementing timers), as this should coordinate
+	     *  better with the server, as it will be more deterministic. */
+	void FixedUpdate();
+
+	/* Handles a new incoming action, by determining if/where to move
+	     * (using checkMove), then sends this action to the server, then
+	     * moves queuedAction up and sets it to null */
+	void HandleAction();
+
+	/* Moves the player's actual position, sets this.transform to it */
+	void MovePlayer();
+
+	/* Determine what Raycast we need to do to see if an obstacle is in the
+	     * way. */
+	bool CheckMove(Vector3 offset);
+
+	/* Start at a point, Raycast in Dir, return false if it hits an obstacle */
+	bool RaycastCheck(Vector3 start, Vector3 Dir);
+
+	/* Change each timer by Time.deltaTime */
+	void FixedUpdateTimers();
+
+	/* Send a move that this player should perform
+	     * set mCurrentAction to it if empty, else overrides mQueuedAction */
+	void AddMove(InputAction newAction);
+
+	/* P only
+	     * Called by the server when this player gets hit */
+	void Incap();
+
+	/* Moves Player back to respawn position */
+	void Respawn( Vector3 Position );
+
+	/* For Later: Allows objects to apply effects on player */
+	void ApplyEffect(Effect e, int value);
 }
---
+++ MainUnityProject/Assets/Scripts/InputManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+	/* Search for input on update by calling helper function*/
+	void Update();    
+	
+	/* (P only) Checks for input */
+	void CheckInput();
+	
+	/* (P only) Applies Input to connected player */
+	void ApplyInput();
+	
+	/* packages needed input to client_SendInput() */
+	void SendInput();
+}
