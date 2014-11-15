391634f5542b016defb5b53019a31abd243e211a
Peter Y. <peteryao2355@gmail.com>
2014-11-14 22:37:13 -0800

    Fixed SQLManager syntax error.

--- MainUnityProject/Assets/Scripts/SQLManager.cs
+++ MainUnityProject/Assets/Scripts/SQLManager.cs
@@ -6,24 +6,15 @@ public class SQLManager : MonoBehaviour {
 	
 	// Use this for initialization
 	public SqlConnection Start () {
-		myConnection = new SqlConnection("user id=username;" + 
+		SqlConnection myConnection = new SqlConnection("user id=username;" + 
 		                                 "password=password;server=serverurl;" + 
 		                                 "Trusted_Connection=yes;" + 
 		                                 "database=database; " + 
 		                                 "connection timeout=30");
 
 		// Try Connecting
-		try
-		{
-			myConnection.Open();
-			return myConnection;
-		}
-
-		// Catch error connecting to SQL DB
-		catch(Exception e)
-		{
-			//Console.WriteLine(e.ToString());
-		}
+		myConnection.Open();
+		return myConnection;
 	}
 	
 	// Update is called once per frame
